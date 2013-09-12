a0f578b9e9289f2bccc344ac32e904bdb28a8033
Rob Close <rob0229@gmail.com>
2013-09-12 09:15:40 -0400

    initial commit

---
+++ wci/src/wci/frontend/MainFunctionProgramThingy.java
@@ -0,0 +1,18 @@
+package wci.frontend;
+
+import wci.frontend.pascal.PascalScanner;
+
+public class MainFunctionProgramThingy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		Scanner scanner = new PascalScanner();
+		
+		
+
+	}
+
+}
